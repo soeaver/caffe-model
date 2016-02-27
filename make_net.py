14bf3a0ccd3e3efcbdb358eb9d9dcbb9a0ab61ee
soeaver <1013608889@qq.com>
2016-02-28 01:19:37 +0800

    run this script to generate train/test prototxt file
    run this script to generate train/test prototxt file

--- make_net.py
+++ make_net.py
@@ -10,8 +10,8 @@ def save_proto(proto, prototxt):
 if __name__ == '__main__':
     model = inception_v3.Inception('imagenet_test_lmdb', 'imagenet_train_lmdb', 1000)
 
-    train_proto = model.inception_v3_proto(32)
-    test_proto = model.inception_v3_proto(32, phase='TEST')
+    train_proto = model.inception_v3_proto(64)
+    test_proto = model.inception_v3_proto(64, phase='TEST')
 
     save_proto(train_proto, 'imagenet_train.prototxt')
     save_proto(test_proto, 'imagenet_test.prototxt')

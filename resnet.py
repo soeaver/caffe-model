bb93062a3cf514bab2487bbc16bf41d1906dbaef
soeaver <1013608889@qq.com>
2016-03-17 14:23:43 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -26,6 +26,17 @@ def factorization_conv_bn_scale_relu(bottom, num_output=64, kernel_size=3, strid
     return conv, conv_bn, conv_scale, conv_relu
 
 
+def factorization_conv_bn_scale(bottom, num_output=64, kernel_size=3, stride=1, pad=0):
+    conv = L.Convolution(bottom, num_output=num_output, kernel_size=kernel_size, stride=stride, pad=pad,
+                         param=[dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)],
+                         weight_filler=dict(type='xavier', std=0.01),
+                         bias_filler=dict(type='constant', value=0.2))
+    conv_bn = L.BatchNorm(conv, use_global_stats=False, in_place=True)
+    conv_scale = L.Scale(conv, scale_param=dict(bias_term=True), in_place=True)
+
+    return conv, conv_bn, conv_scale
+
+
 def eltwize_relu(bottom1, bottom2):
     residual_eltwise = L.Eltwise(bottom1, bottom2, eltwise_param=dict(operation=1))
     residual_eltwise_relu = L.ReLU(residual_eltwise, in_place=True)
@@ -45,14 +56,14 @@ def residual_branch(bottom, base_output=64):
         factorization_conv_bn_scale_relu(bottom, num_output=base_output, kernel_size=1)  # base_output x n x n
     branch2b, branch2b_bn, branch2b_scale, branch2b_relu = \
         factorization_conv_bn_scale_relu(branch2a, num_output=base_output, kernel_size=3, pad=1)  # base_output x n x n
-    branch2c, branch2c_bn, branch2c_scale, branch2c_relu = \
-        factorization_conv_bn_scale_relu(branch2b, num_output=4 * base_output, kernel_size=1)  # 4*base_output x n x n
+    branch2c, branch2c_bn, branch2c_scale = \
+        factorization_conv_bn_scale(branch2b, num_output=4 * base_output, kernel_size=1)  # 4*base_output x n x n
 
     residual, residual_relu = \
         eltwize_relu(bottom, branch2c)  # 4*base_output x n x n
 
     return branch2a, branch2a_bn, branch2a_scale, branch2a_relu, branch2b, branch2b_bn, branch2b_scale, branch2b_relu, \
-           branch2c, branch2c_bn, branch2c_scale, branch2c_relu, residual, residual_relu
+           branch2c, branch2c_bn, branch2c_scale, residual, residual_relu
 
 
 def residual_branch_shortcut(bottom, stride=2, base_output=64):
@@ -63,36 +74,33 @@ def residual_branch_shortcut(bottom, stride=2, base_output=64):
     :param bottom: bottom layer
     :return: layers
     """
-    branch1, branch1_bn, branch1_scale, branch1_relu = \
-        factorization_conv_bn_scale_relu(bottom, num_output=4 * base_output, kernel_size=1, stride=stride)
+    branch1, branch1_bn, branch1_scale = \
+        factorization_conv_bn_scale(bottom, num_output=4 * base_output, kernel_size=1, stride=stride)
 
     branch2a, branch2a_bn, branch2a_scale, branch2a_relu = \
         factorization_conv_bn_scale_relu(bottom, num_output=base_output, kernel_size=1, stride=stride)
     branch2b, branch2b_bn, branch2b_scale, branch2b_relu = \
         factorization_conv_bn_scale_relu(branch2a, num_output=base_output, kernel_size=3, pad=1)
-    branch2c, branch2c_bn, branch2c_scale, branch2c_relu = \
-        factorization_conv_bn_scale_relu(branch2b, num_output=4 * base_output, kernel_size=1)
+    branch2c, branch2c_bn, branch2c_scale = \
+        factorization_conv_bn_scale(branch2b, num_output=4 * base_output, kernel_size=1)
 
     residual, residual_relu = \
         eltwize_relu(branch1, branch2c)  # 4*base_output x n x n
 
-    return branch1, branch1_bn, branch1_scale, branch1_relu, branch2a, branch2a_bn, branch2a_scale, branch2a_relu, \
-           branch2b, branch2b_bn, branch2b_scale, branch2b_relu, branch2c, branch2c_bn, branch2c_scale, branch2c_relu, \
-           residual, residual_relu
+    return branch1, branch1_bn, branch1_scale, branch2a, branch2a_bn, branch2a_scale, branch2a_relu, branch2b, \
+           branch2b_bn, branch2b_scale, branch2b_relu, branch2c, branch2c_bn, branch2c_scale, residual, residual_relu
 
 
-branch_shortcut_string = 'n.res(stage)a_branch1, n.res(stage)a_branch1_bn, n.res(stage)a_branch1_scale, n.res(stage)a_branch1_relu, \
+branch_shortcut_string = 'n.res(stage)a_branch1, n.res(stage)a_branch1_bn, n.res(stage)a_branch1_scale, \
         n.res(stage)a_branch2a, n.res(stage)a_branch2a_bn, n.res(stage)a_branch2a_scale, n.res(stage)a_branch2a_relu, \
         n.res(stage)a_branch2b, n.res(stage)a_branch2b_bn, n.res(stage)a_branch2b_scale, n.res(stage)a_branch2b_relu, \
-        n.res(stage)a_branch2c, n.res(stage)a_branch2c_bn, n.res(stage)a_branch2c_scale, n.res(stage)a_branch2c_relu, \
-        n.res(stage)a, n.res(stage)a_relu = \
+        n.res(stage)a_branch2c, n.res(stage)a_branch2c_bn, n.res(stage)a_branch2c_scale, n.res(stage)a, n.res(stage)a_relu = \
             residual_branch_shortcut((bottom), stride=(stride), base_output=(num))'
 
 branch_string = 'n.res(stage)b(order)_branch2a, n.res(stage)b(order)_branch2a_bn, n.res(stage)b(order)_branch2a_scale, \
         n.res(stage)b(order)_branch2a_relu, n.res(stage)b(order)_branch2b, n.res(stage)b(order)_branch2b_bn, \
         n.res(stage)b(order)_branch2b_scale, n.res(stage)b(order)_branch2b_relu, n.res(stage)b(order)_branch2c, \
-        n.res(stage)b(order)_branch2c_bn, n.res(stage)b(order)_branch2c_scale, n.res(stage)b(order)_branch2c_relu, \
-        n.res(stage)b(order), n.res(stage)b(order)_relu = \
+        n.res(stage)b(order)_branch2c_bn, n.res(stage)b(order)_branch2c_scale, n.res(stage)b(order), n.res(stage)b(order)_relu = \
             residual_branch((bottom), base_output=(num))'
 
 
